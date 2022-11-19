ce1e6bc2d6c27f1f5349fc6ff2c0415be31a61cb
Ygolec <thekevindit@gmail.com>
2022-11-19 17:56:06 +0700

    1.0

---
+++ components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="self-center ml-2 text-2xl font-extrabold text-gray-900 whitespace-nowrap dark:text-white">
+    <img src="/logo.png" width="150" height="150">
+  </span>
+</template>
